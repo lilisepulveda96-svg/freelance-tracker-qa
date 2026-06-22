5e02dac5b3219689ec6f3e3a7acbebb2782ae88c
lili <lili.sepulveda.96@gmail.com>
2026-06-22 20:08:22 +0200

    feat(auth): implement negative cases and form validation specs [QA-201]

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -2,6 +2,7 @@ class LoginPage {
   #emailInput = 'input[name="username"]';
   #passwordInput = 'input[name="password"]';
   #submitBtn = 'button[type="submit"]';
+  #errorMessage = 'div[role="alert"]';
 
   visit() {
     cy.visit("#/login");
@@ -15,8 +16,32 @@ class LoginPage {
     return this;
   }
 
+  submitEmpty() {
+    cy.get(this.#submitBtn).click();
+    return this;
+  }
+
   assertLoggedIn() {
     cy.url().should("not.include", "/login");
+    return this;
+  }
+
+  assertStillOnLogin() {
+    cy.url().should("include", "/login");
+    return this;
+  }
+
+  assertElementsVisible() {
+    cy.get(this.#emailInput).should("be.visible");
+    cy.get(this.#passwordInput).should("be.visible");
+    cy.get(this.#submitBtn).should("be.visible");
+    return this;
+  }
+
+  assertErrorMessageVisible(expectedText) {
+    cy.get(this.#errorMessage)
+      .should("be.visible")
+      .and("contain.text", expectedText);
     return this;
   }
 }
